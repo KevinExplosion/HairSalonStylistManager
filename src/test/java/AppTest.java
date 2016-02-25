12b3f9c94a03e803a952eafbf522a8354673206f
kevin <mattisonii@yahoo.com>
2016-02-25 15:51:46 -0800

    Pass first integration test.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,15 +1,17 @@
-import org.junit.*;
-import static org.junit.Assert.*;
-import java.util.ArrayList;
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.junit.Rule;
+
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
   public WebDriver getDefaultDriver() {
       return webDriver;
   }
@@ -17,9 +19,12 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-    @Test
-    public void rootTest() {
-        goTo("http://localhost:4567/change");
-        assertThat(pageSource()).contains("Salon Scheduler");
-    }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Salon Scheduler");
+  }
 }
