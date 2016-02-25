7db2199fdd024cce298d9d596a9ba1aa1578e157
kevin <mattisonii@yahoo.com>
2016-02-25 15:33:25 -0800

    Begin testing front end specs.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -17,17 +17,9 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-    // @Test
-    // public void rootTest() {
-    //     goTo("http://localhost:4567/change");
-    //     assertThat(pageSource()).contains("Input the number of cents");
-    // }
-
-    // @Test
-    // public void fillFormTest() {
-    //     goTo("http://localhost:4567/change");
-    //     fill("#changeInput").with("33");
-    //     submit("#submit");
-    //     assertThat(pageSource()).contains("Your change for");
-    // }
+    @Test
+    public void rootTest() {
+        goTo("http://localhost:4567/change");
+        assertThat(pageSource()).contains("Input the number of cents");
+    }
 }
